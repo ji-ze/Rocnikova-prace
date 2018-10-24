95cbaf22e8bed78b983deb8af0c755ab07614dfe
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-10-24 21:38:59 +0200

    ver. 3
    oprava indexu iterace

--- pi_Arch – kopie – kopie.py
+++ pi_Arch – kopie – kopie.py
@@ -5,9 +5,9 @@ S=2*r
 s=(2*r)/2**0.5
 k=4
 
-print("n, e, E;")
+print("n, e, E")
 for i in range(26):
-  print("{}, {}, {};".format(i+1, abs(math.pi-(s*k)/(2*r)), abs(math.pi-(S*k)/(2*r))))
+  print("{}, {}, {};".format(k, abs(math.pi-(s*k)/(2*r)), abs(math.pi-(S*k)/(2*r))))
   S=2*S/(2+(4+S**2)**0.5)
   s=(s**2/(2+(4-s**2)**0.5))**0.5
   k*=2
