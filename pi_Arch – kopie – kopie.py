ee8e4fb0c416256d24ee78cc6d947cb4b736861e
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-10-24 21:21:09 +0200

    ver. 2
    správná metoda

--- pi_Arch – kopie – kopie.py
+++ pi_Arch – kopie – kopie.py
@@ -1,13 +1,13 @@
+import math
+
 r=1
 S=2*r
 s=(2*r)/2**0.5
 k=4
 
-print("n, e, E")
+print("n, e, E;")
 for i in range(26):
-  SS=2*S/(2+(4+S**2)**0.5)
-  ss=(s**2/(2+(4-s**2)**0.5))**0.5
+  print("{}, {}, {};".format(i+1, abs(math.pi-(s*k)/(2*r)), abs(math.pi-(S*k)/(2*r))))
+  S=2*S/(2+(4+S**2)**0.5)
+  s=(s**2/(2+(4-s**2)**0.5))**0.5
   k*=2
-  print("{}, {}, {}".format(i+2, abs((s*(k/2))/(2*r)-(ss*k)/(2*r)), abs((S*(k/2))/(2*r)-(SS*k)/(2*r))))
-  s=ss
-  S=SS
