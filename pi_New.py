6a0295df313b07e78166924028402b0cd77708c7
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-03 21:25:31 +0100

    dalsi 4

---
+++ pi_New.py
@@ -0,0 +1,13 @@
+import math
+def fac(n):
+   if n == 0:
+       return 1
+   else:
+       return n*fac(n-1)
+
+x=0
+
+print("n, p")
+for i in range(25):
+  x+=fac(2*i)/(2**(4*i+1)*fac(i)**2*(2*i+1))
+  print("{}, {}".format(i+1, abs(6*x-math.pi)))
