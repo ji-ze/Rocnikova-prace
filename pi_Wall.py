2fabd803e7054157a448981e1723cd5d1abbb082
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-09 20:32:31 +0100

    Mem

--- pi_Wall.py
+++ pi_Wall.py
@@ -1,9 +1,16 @@
 import math
 
 x=1
+i=1
+x*=4*i**2/(4*i**2-1)
+pp=abs(2*x-math.pi)
+mem=0
 
-print("n, p")
-for i in range(1,50):
+print("n, mem")
+for i in range(2,1000000+2):
+    p=pp
     x*=4*i**2/(4*i**2-1)
-    print("{}, {}".format(i, abs(2*x-math.pi)))
-
+    pp=abs(2*x-math.pi)
+    mem+=(p-pp)/8
+    
+print("{}, {}".format(i-1, mem))
