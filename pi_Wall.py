b97ae762cb296a916e97b0ba2844967d45cf0dcd
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-14 20:52:06 +0100

    v. 2

--- pi_Wall.py
+++ pi_Wall.py
@@ -2,8 +2,10 @@ import math
 
 x=1
 
-print("n, p")
-for i in range(1,50):
+out_file=open("Wall.csv", "w")
+out_file.write("n, p\n")
+for i in range(1,64):
     x*=4*i**2/(4*i**2-1)
-    print("{}, {}".format(i, abs(2*x-math.pi)))
+    out_file.write("{}, {}\n".format(i, abs(2*x-math.pi)))
 
+out_file.close()
