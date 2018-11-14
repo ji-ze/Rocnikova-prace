528ae5ed128364c79d528f995e7aa1962bee77e6
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-14 20:49:28 +0100

    v. 2

--- pi_Leib.py
+++ pi_Leib.py
@@ -2,7 +2,10 @@ import math
 
 x=0
 
-print("n, pi")
-for i in range(50):
+out_file=open("Leib.csv", "w")
+out_file.write("n, p\n")
+for i in range(64):
   x=x+(((-1)**i)/(2.0*i+1))
-  print("{}, {}".format(i+1, abs(x*4-math.pi)))
+  out_file.write("{}, {}\n".format(i+1, abs(x*4-math.pi)))
+
+out_file.close()
