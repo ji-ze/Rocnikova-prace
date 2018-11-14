18d89dc8bd479713e4dd9c01a6752748217db67e
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-14 20:44:46 +0100

    v. 2

--- pi_Desc.py
+++ pi_Desc.py
@@ -1,13 +1,16 @@
 import math
+from decimal import *
+getcontext().prec = 24
 
 r=1/4
-R=(2**0.5)/4
-n=4
+R=(Decimal(2).sqrt())/4
 
-print("n, e, E")
-for i in range(26):
-  print("{}, {}, {};".format(n, abs(math.pi-1/R), abs(math.pi-1/r)))
-  rr=(R+r)/2
-  R=((R*(r+R))/2)**0.5
-  r=rr
-  n*=2
+out_file=open("Desc.csv", "w")
+out_file.write("n, p, P\n")
+for n in range(64):
+  out_file.write("{}, {}, {}\n".format(n+1, abs(Decimal(math.pi)-Decimal(1/R)), abs(Decimal(math.pi)-Decimal(1/r))))
+  G=(Decimal(R)+Decimal(r))/2
+  R=Decimal((R*(Decimal(r)+Decimal(R)))/2).sqrt()
+  r=G
+  
+out_file.close()
