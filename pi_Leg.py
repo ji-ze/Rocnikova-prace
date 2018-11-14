5c6afc70d88d24bfabf889395d1df8cbf1fec9c5
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-14 20:48:43 +0100

    v. 2

--- pi_Leg.py
+++ pi_Leg.py
@@ -1,15 +1,19 @@
 import math
+from decimal import *
+getcontext().prec = 24
 
 a=1
-b=1/2**0.5
+b=Decimal(1/2**0.5)
 s=1/4
 
-print("n, p, P")
-for n in range(10):
-  aa=(a+b)/2
-  b=(a*b)**0.5
-  c=a-aa
-  print("{}, {}, {}".format(n, abs(aa**2/s-math.pi), abs(a**2/s-math.pi)))
-  a=aa
-  s=s-(2**n)*c**2
+out_file=open("Leg.csv", "w")
+out_file.write("n, p, P\n")
+for n in range(64):
+  aa=(Decimal(a)+Decimal(b))/2
+  b=Decimal(a*b).sqrt()
+  c=Decimal(a)-Decimal(aa)
+  out_file.write("{}, {}, {}\n".format(n, abs(Decimal(aa**2)/Decimal(s)-Decimal(math.pi)), abs(Decimal(a**2)/Decimal(s)-Decimal(math.pi))))
+  a=Decimal(aa)
+  s=Decimal(s)-Decimal((2**n)*c**2)
   
+out_file.close()
