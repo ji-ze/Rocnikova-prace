e43845d5053649c9cbbb5cad92e425069df29f5a
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-14 20:48:02 +0100

    v. 2

--- pi_Kus.py
+++ pi_Kus.py
@@ -1,11 +1,15 @@
 import math
+from decimal import *
+getcontext().prec = 24
 
 r=1/4
-R=(2**0.5)/4
-n=4
-print("n, p, P")
-for i in range(26):
-  print("{}, {}, {}".format(n, abs(1/R-math.pi), abs(1/r-math.pi)))
-  r=(R+r)/2
-  R=(R*r)**0.5
-  n*=2
+R=Decimal((2**0.5)/4)
+
+out_file=open("Kus.csv", "w")
+out_file.write("n, p, P\n")
+for n in range(64):
+  out_file.write("{}, {}, {}\n".format(n+1, abs(Decimal(1/R)-Decimal(math.pi)), abs(Decimal(1/r)-Decimal(math.pi))))
+  r=(Decimal(R)+Decimal(r))/2
+  R=Decimal(R*r).sqrt()
+
+out_file.close()
