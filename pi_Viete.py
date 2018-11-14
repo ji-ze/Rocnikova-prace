2de1aa186bdfa1147126ba13e683cb53c6783680
ji-ze <44098973+ji-ze@users.noreply.github.com>
2018-11-14 20:51:39 +0100

    v. 2

--- pi_Viete.py
+++ pi_Viete.py
@@ -1,10 +1,15 @@
 import math
+from decimal import *
+getcontext().prec = 24
 
-print("n, p")
-pi=2
-for i in range(1,30):
-    x=0
-    print("{}, {}".format(i+1, abs(pi-math.pi)))
+out_file=open("Viete.csv", "w")
+out_file.write("n, p\n")
+pi=Decimal(2)
+for i in range(1,64):
+    x=Decimal(0)
+    out_file.write("{}, {}\n".format(i, abs(Decimal(pi)-Decimal(math.pi))))
     for j in range(i):
-        x=(2+x)**0.5
-    pi=pi*2/(x)
+        x=(2+Decimal(x))**Decimal(0.5)
+    pi=Decimal(pi*2/x)
+
+out_file.close()
